178bcce31474eaeda0a382f2d3c47f5bf01c03cf
Arques <bhuvaneshkpb@gmail.com>
2026-05-24 10:56:26 +0530

    kafka configurations added

--- kafka_streams/src/main/java/com/bhuvanesh/kafka_streams/config/KafkaStreamConfig.java
+++ kafka_streams/src/main/java/com/bhuvanesh/kafka_streams/config/KafkaStreamConfig.java
@@ -1,5 +1,25 @@
 package com.bhuvanesh.kafka_streams.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
 public class KafkaStreamConfig {
-    
+    @Bean
+    public NewTopic createTransactionTopic(){
+        return TopicBuilder.name("transaction")
+                        .partitions(3)
+                        .replicas(1)
+                        .build();
+    }
+
+    @Bean
+    public NewTopic createFraudAlertTopic() {
+        return TopicBuilder.name("fraud-alert")
+                        .partitions(3)
+                        .replicas(2)
+                        .build();
+    }
 }
