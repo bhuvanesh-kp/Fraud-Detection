ad99c6ca890829dbd91c8d1a67e9076313983f33
Arques <bhuvaneshkpb@gmail.com>
2026-05-24 11:19:58 +0530

    kafka serializer added

--- kafka_streams/src/main/java/com/bhuvanesh/kafka_streams/serializer/TansactionSerializer.java
+++ kafka_streams/src/main/java/com/bhuvanesh/kafka_streams/serializer/TansactionSerializer.java
@@ -1,5 +1,21 @@
 package com.bhuvanesh.kafka_streams.serializer;
 
-public class TansactionSerializer {
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.Serializer;
+
+import com.bhuvanesh.kafka_streams.events.Transaction;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class TansactionSerializer implements Serializer<Transaction>{
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public byte[] serialize(String topic, Transaction data) {
+        try {
+            return mapper.writeValueAsBytes(data);
+        } catch (Exception e) {
+            throw new SerializationException("Error serializing transaction: ", e);
+        }
+    }
     
 }
